9a16fa8a7f388eae3603a7d0185594c2ca78ea53
kismatthapamagar1 <000kismatthapa@gmail.com>
2026-07-28 20:53:05 +0545

    minor update in mistake

---
+++ basics/stack.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<stack>
+using namespace std;
+
+void printStackElements(stack<int> st) {
+	while (!st.empty()) {
+		cout << st.top() << endl;
+		st.pop();
+	}
+}
+
+int main()
+{	//empty, size, push, pop, top
+	stack<int>numbersStack;
+	numbersStack.push(1);
+	numbersStack.push(2);
+	numbersStack.push(3);
+	numbersStack.pop();
+		
+	printStackElements(numbersStack);
+
+	if (numbersStack.empty())
+		cout << "Stack is empty"<<endl;
+	else
+		cout << "Stack is not empty" << endl;
+	cout << "Stack size is " << numbersStack.size() << endl;
+
+	system("pause>0");
+}
+
